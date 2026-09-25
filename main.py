a3c408111571c6714339c439b96b4c6495e2d2cd
Manas249 <manaskumarpradham@gmail.com>
2026-09-26 01:08:14 +0530

    again new

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ while True:
     try:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
+        result = add(num1, num2)
 
     except ValueError:
         print("❌ Invalid input! Please enter numbers only.")
